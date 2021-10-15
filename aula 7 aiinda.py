bd47ba94aa402f511a857a59e6679b833f7b1a0f
Guilherme Neres <92585439+GuilhermeNeres@users.noreply.github.com>
2021-10-15 17:54:16 -0300

    Delete aula 7 aiinda.py

--- aula 7 aiinda.py
+++
@@ -1,8 +0,0 @@
-n1 = int(input('digite um valor'))
-n2 = int(input('digite outro valor'))
-print(f'a soma entre {n1} e {n2} é igual a {n1+n2}')
-print(f'a multiplicação entre {n1} e {n2} é igual a {n1*n2}')
-print(f'a subtraçao entre {n1} e{n2} é igual a {n1-n2}')
-print(f'a potenciacao de {n1} elevado a {n2} é igual a {n1**n2 :.3f}')
-print(f'a divisão entre `{n1} por {n2} é igual a {n1/n2}')
-
